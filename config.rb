cf6b037815a5f50175286b0c658be386313ca6cd
AlexGorski <alex.gorski@gmail.com>
2015-05-12 19:58:28 -0400

    update deploy remote for new org repo

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 activate :deploy do |deploy|
   deploy.method = :git
   # Optional Settings
-  deploy.remote   = 'https://github.com/litigation/litigation.github.io.git' # remote name or git url, default: origin
+  deploy.remote   = 'https://github.com/litigation-jobs/litigation-jobs.github.io.git' # remote name or git url, default: origin
   deploy.branch   = 'master' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
